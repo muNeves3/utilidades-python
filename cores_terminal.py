8d594134d2420aeb0de34b12fc5dad87ac0d1d36
Murilo de Souza Neves <murilogrilosn@gmail.com>
2020-06-17 21:58:51 -0300

    mudança de cores no texto

--- cores_terminal.py
+++ cores_terminal.py
@@ -8,4 +8,5 @@ print('\033[mHello, World!\033[m')        #fundo padrão letra branca
 print('\033[7;30mHello, World!\033[m')    #fundo branco letra preta(invertido)
 
 nome = 'Murilo'
+
 print('Olá, muito prazer em te conhecer, {}{}{}'.format('\033[4;34m',nome,'\033[m'))
